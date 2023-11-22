6063703bf059b367a7024235c5673e957afd74cc
by-moan <moanbynanachi@gmail.com>
2023-11-21 21:34:50 -0700

    update

--- preload.js
+++ preload.js
@@ -1,7 +1,18 @@
-/*
-  Preload
+/**
+ * The preload script runs before. It has access to web APIs
+ * as well as Electron's renderer process modules and some
+ * polyfilled Node.js functions.
+ *
+ * https://www.electronjs.org/docs/latest/tutorial/sandbox
  */
+
+const { contextBridge, ipcRenderer } = require('electron');
 
 document.addEventListener('DOMCONTENTLOADED', () => {
   
+});
+
+
+contextBridge.exposeInMainWorld('thing', {
+  func: () => ipcRenderer.send('alert_main')
 });
